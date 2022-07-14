c94ce7c9220520fd05de77d2598312434285c139
Jeet Patel <jeet63240@gmail.com>
2022-07-14 21:22:53 +0530

    Write 1st JavaScript Program.

---
+++ 01_HelloWorld.js
@@ -0,0 +1,11 @@
+// Outputs a message to the console
+console.log("Hello World");
+
+// Outputs a warning message to the console
+console.warn("This is Warning!");
+
+// Outputs an error message to the console
+console.error("This is Error!");
+
+// Outputs an informational message to the console
+console.info("This is Info");
