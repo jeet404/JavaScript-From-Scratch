487d5d982ee896f068ff01280a2bc35d6499064e
Jeet Patel <jeet63240@gmail.com>
2022-07-17 18:08:23 +0530

    Add variables.js

---
+++ 01/01_variables.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+// JavaScript is a dynamically typed language
+let a = 45;
+a = 98;
+a = "Jeet"; // Dynamic Typing: You can always change the type of a variable
+console.log(a);
+let b = "Jeet";
+// let 56c = "Patel"  // This will throw an error  because variable name cannot start with a number
