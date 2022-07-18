61b45ee484342144841a364e10c98efc9eea1475
Jeet Patel <jeet63240@gmail.com>
2022-07-18 20:24:09 +0530

    Add code for let, var and const.

---
+++ 01/02_variables.js
@@ -0,0 +1,15 @@
+console.log("In JavaScript , let and const");
+// var a = 45;
+// var a = "p"
+let b = "Jeet";
+const author = "Jeet";
+// author = 5; // Throws an error because constant cannot be changed
+b = 4;
+const harry = 0;
+let c = null;
+let d = undefined;
+{
+  let b = "this";
+  console.log(b);
+}
+console.log(b);
