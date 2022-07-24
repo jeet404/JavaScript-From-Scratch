e7454cfdc348c5d19c206d81238260f313450694
Jeet Patel <jeet63240@gmail.com>
2022-07-24 20:11:46 +0530

    Add code file for Switch statement.

---
+++ 02/03_SwitchStatement.js
@@ -0,0 +1,16 @@
+let a = 18;
+switch (a) {
+  case 0:
+    console.log("This is an invalid age");
+    break;
+  case 9:
+    console.log("You are a kid and you cannot even think of driving");
+    break;
+  case 18:
+    console.log("You can now drive as you are above 18");
+    break;
+  default:
+    console.log("You can now drive as you are above 18");
+    break;
+}
+console.log("Done");
