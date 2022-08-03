488fb9600d28c8c071e375e83dd4d1b8270c899f
Jeet Patel <jeet63240@gmail.com>
2022-08-03 18:52:07 +0530

    Add code for strings.

---
+++ 04/01_Strings.js
@@ -0,0 +1,21 @@
+let name = "Jeet";
+// console.log(name.length)
+console.log(name[0]);
+console.log(name[1]);
+let friend = "Pooja";
+console.log(friend);
+// let myfriend = "Saavan' // Dont do this
+// console.log(myfriend)
+
+// Template literals
+let boy1 = "Parimal";
+let boy2 = "Yash";
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`;
+console.log(sentence);
+
+// Escape Sequence Characters
+// let fruit = 'Bana\'na'
+// console.log(fruit)
+let fruit = 'Bana"na';
+console.log(fruit);
