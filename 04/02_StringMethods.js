63611b3299d8b568b83ab8f95cf2cf6afe02c989
Jeet Patel <jeet63240@gmail.com>
2022-08-04 19:43:59 +0530

    Add String Methods code.

---
+++ 04/02_StringMethods.js
@@ -0,0 +1,23 @@
+let name = "Jeet";
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2, 4))
+// console.log(name.slice(2))
+// console.log(name.replace("Har", "Per"))
+// let friend = "Samay"
+// console.log(name.concat(" is a friend of ", friend, " Ok"))
+// let friend2 = "      Meena        "
+// console.log(friend2)
+// console.log(friend2.trim())
+let fr = "Pooja" + "Shivika" + "Riya";
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+// fr[4]= "o" // This is not possible
+console.log(fr);
+// Use a for loop to print a string
+for (let i = 0; i < fr.length; i++) {
+  console.log(fr[i]);
+}
