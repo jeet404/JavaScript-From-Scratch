58f525c456b550cb52ba75aceeabde1fe8e20f99
Jeet Patel <jeet63240@gmail.com>
2022-08-10 21:30:52 +0530

    Add code file for more array methods.

---
+++ 05/03_ArrayMethod2.js
@@ -0,0 +1,19 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+let num_even_more = [211, 212, 213, 214, 415, 416, 417, 418, 419];
+console.log(num.length);
+delete num[0];
+console.log(num.length);
+
+let newArray = num.concat(num_more, num_even_more);
+console.log(newArray);
+console.log(num, num_more);
+
+// sort method
+let compare = (a, b) => {
+  return b - a;
+};
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229];
+num.sort(compare);
+num.reverse();
+console.log(num);
